ac3452468e7fcc0f1064a5c02295f07f6c77c9bc
sscheiner <sf.scheiner@gmail.com>
2018-02-23 23:07:01 -0500

    test update
    add testRemoveItem and testInsertMoney

--- VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -12,7 +12,7 @@ public class VendingMachineTest {
 	VendingMachineItem item1, item2, item3;
 	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
 		vend1 = new VendingMachine();
 		vend2 = new VendingMachine();
 		vend3 = new VendingMachine();
@@ -21,10 +21,68 @@ public class VendingMachineTest {
 		item3 = new VendingMachineItem("josh", 100);
 	}
 
+	/**
+	 * test that item adds are processed and checked correctly
+	 * @throws Exception
+	 */
 	@Test
 	public void testAddItem(){
+		vend1.addItem(item1, "A");
+		vend1.addItem(item2, "B");
+		vend1.addItem(item3, "C");
+		vend1.addItem(item1, "D");
+		//catch invalid slot
+		try {
+			vend2.addItem(item1, "Z");
+			fail("exception not thrown");
+		}
+		catch(VendingMachineException e) {
+			//test succeeds
+		}
+		
+		//catch adding to occupied slot
+		try {
+			vend1.addItem(item1, "B");
+			fail("exception not thrown");
+		}
+		catch(VendingMachineException e) {
+			//test succeeds
+		}
+
+	}
+	
+	/**
+	 * confirm proper removal of item from vending machine slot
+	 * @throws Exception
+	 */
+	@Test
+	public void testRemoveItem() throws Exception{
+		
+		vend3.addItem(item1, "A");
+		vend3.removeItem("A");
+		assertNull(vend3.getItem("A"));
 		
 	}
+	
+	
+	/**
+	 * tests money insertion method for checks and proper behavior
+	 * @throws Exception
+	 */
+	@Test
+	public void testInsertMoney(){
+		vend1.insertMoney(99);
+		assertEquals(vend1.getBalance(), 99, 0.000001);
+		//try to insert negative money
+		try {
+			vend1.insertMoney(-46.2);
+			fail("exception not thrown");
+		}
+		catch(VendingMachineException e) {
+			//test succeeds
+		}	
+	}
+
 	
 	/**
 	 * not strictly necessary
@@ -33,11 +91,6 @@ public class VendingMachineTest {
 	@After
 	public void tearDown() throws Exception {
 		
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
 	}
 
 }
